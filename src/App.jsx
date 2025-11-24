627e1cc539aac82a1c50f799a7555058d79f16b3
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-24 20:55:15 +0600

    logo added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 const App = () => {
   return (
     <>
-    <div>dcshacd</div>
-    
+      <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500">
+        <div className="flex h-screen overflow-hidden">
+          <Sidebar />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
