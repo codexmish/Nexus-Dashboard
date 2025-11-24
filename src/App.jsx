9d9ac4204fb8badbd62462cffb00e6567a18cb30
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-24 21:26:21 +0600

    header loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 
 const App = () => {
   return (
@@ -8,6 +9,9 @@ const App = () => {
       <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50 dark:from-slate-900 dark:via-slate-800 dark:to-slate-900 transition-all duration-500">
         <div className="flex h-screen overflow-hidden">
           <Sidebar />
+          <div className="flex-1 flex flex-col overflow-hidden">
+            <Header/>
+          </div>
         </div>
       </div>
     </>
