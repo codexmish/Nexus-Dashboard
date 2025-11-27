0b7f88b6912a7ecb2eb40b1458966bf05fdf5d82
codexmish <majharulislam.shanto.70@gmail.com>
2025-11-27 20:16:46 +0600

    dark mode toggle  added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,24 @@ import {
   ChevronDown,
   Filter,
   Menu,
+  Moon,
   Plus,
   Search,
   Settings,
   Sun,
 } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 import user from "../assets/images/user.jpg";
 
-const Header = ({sidebarCollaps, onToggleSidebar}) => {
+const Header = ({ sidebarCollaps, onToggleSidebar }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleDark = () => {
+    document.documentElement.classList.toggle("dark");
+    setDarkMode(!darkMode);
+  };
+  console.log(darkMode);
+
   return (
     <>
       <div className="bg-white/80 dark:bg-slate-900/80 backdrop-blur-xl border-b border-slate-200/50 dark:border-slate-700/50 px-6 py-4">
@@ -19,7 +28,10 @@ const Header = ({sidebarCollaps, onToggleSidebar}) => {
           {/* ---left section */}
 
           <div className="flex items-center space-x-4">
-            <button onClick={onToggleSidebar} className="p-2 rounded-lg text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors">
+            <button
+              onClick={onToggleSidebar}
+              className="p-2 rounded-lg text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors"
+            >
               <Menu className="w-5 h-5" />
             </button>
 
@@ -57,8 +69,16 @@ const Header = ({sidebarCollaps, onToggleSidebar}) => {
             </button>
 
             {/* Toggle */}
-            <button className="p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors">
-              <Sun className="w-5 h-5" />
+            <button
+              onClick={handleDark}
+              className="p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-100 dark:hover:bg-slate-800 transition-colors"
+            >
+              {!darkMode ? (
+                <Sun className="w-5 h-5" />
+              ) : (
+                <Moon className="w-5 h-5" />
+              )}
+              {/* <Sun className="w-5 h-5" /> */}
             </button>
 
             {/* ---Notification */}
